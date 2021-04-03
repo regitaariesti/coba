e64452f7a6307a1c465903fea01d6b9bb411907e
regitaariesti <regitaariestis@gmail.com>
2021-04-03 11:21:31 +0800

    memberikan comment pada web.php

--- tubes/routes/web.php
+++ tubes/routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/anggota','\App\Http\Controllers\AnggotaController');
+Route::resource('/anggota','\App\Http\Controllers\AnggotaController'); #route untuk anggota controller
 Route::resource('/transaksi-simpanan','\App\Http\Controllers\TransaksiSimpananController');
 Route::resource('/manajemen-bunga','\App\Http\Controllers\ManajemenBungaController');
 Route::resource('/perhitungan-bunga','\App\Http\Controllers\PerhitunganBungaController');
